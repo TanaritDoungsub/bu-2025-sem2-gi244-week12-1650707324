e9ed99dbb31bc80f921b877f909431f3b7dc2ae3
TanaritDoungsub <161479680+TanaritDoungsub@users.noreply.github.com>
2026-04-03 17:23:04 +0700

    W12 lecture

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,6 +1,7 @@
+using System.Drawing;
 using UnityEngine;
 using UnityEngine.EventSystems;
-
+ 
 /// <summary>
 /// IPointerClickHandler interface is used to detect pointer click events.
 /// This is part of Unity's Event System.
@@ -13,38 +14,63 @@ public class Target : MonoBehaviour, IPointerClickHandler
     private float xRange = 4;
     private float ySpawnPos = -6;
     private Rigidbody rb;
-
+ 
     public int point;
     public ParticleSystem explosionParticle;
-
+    private readonly object gm;
+ 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.AddForce(RandomForce(), ForceMode.Impulse);
+        rb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque());
+        transform.position = RandomSpawnPos();
     }
-
+ 
     Vector3 RandomForce()
     {
         return Vector3.up * Random.Range(minSpeed, maxSpeed);
     }
-
+ 
     float RandomTorque()
     {
         return Random.Range(-maxTorque, maxTorque);
     }
-
+ 
     Vector3 RandomSpawnPos()
     {
         return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
-
+ 
     // NOTE: OnPointerClick is part of IPointerClickHandler interface
     public void OnPointerClick(PointerEventData eventData)
     {
-
+        Debug.Log("Clicked");
+        Destroy(this.gameObject);
+ 
+        var gm = FindAnyObjectByType<GameManager>();
+        gm.UpdateScore(point);
+ 
+        Instantiate(explosionParticle, transform.position, Quaternion.identity);
     }
-
+ 
     private void OnTriggerEnter(Collider other)
     {
-
+        if (other.CompareTag("Sensor"))
+        {
+            var gm = FindAnyObjectByType<GameManager>();
+            gm.UpdateScore(-point);
+            Destroy(this.gameObject);
+ 
+            if (gameObject.CompareTag("Bad"))
+            {
+                if (gm != null)
+                {
+                    gm.UpdateScore(point);
+                }
+            }
+ 
+            Destroy(this.gameObject);
+        }
     }
 }
